f4961adab930feadb31d0c12713330359af314a4
Ruslan1908 <92510484+Ruslan1908@users.noreply.github.com>
2025-06-10 19:31:33 +0200

    Update settings.py
    rfactor

--- Star Jet/settings.py
+++ Star Jet/settings.py
@@ -1,6 +1,19 @@
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 640
-FPS = 60
-PLAYER_SPEED = 5
-BULLET_SPEED = 7
-ENEMY_SPEED = 2
+SCREEN_WIDTH        = 480
+SCREEN_HEIGHT       = 640
+FPS                 = 60
+
+PLAYER_SPEED        = 5
+BULLET_SPEED        = 7
+ENEMY_SPEED         = 2
+
+# ile dzielimy wymiary PNG, aby zmniejszyć
+SCALE_FACTOR        = 3
+
+# zdrowie
+PLAYER_MAX_HEALTH   = 5
+ENEMY_MAX_HEALTH    = 3
+
+# PowerUp
+POWERUP_TYPES       = ['health', 'homing']
+POWERUP_SPEED       = 2
+BONUS_SPAWN_TICKS   = FPS * 10  # co ~10 sekund
